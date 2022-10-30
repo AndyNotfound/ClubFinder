11ed266404a1dd0940a5116bc0bf0374dacf738d
AndyNotfound <Andy.Notfound@gmail.com>
2022-10-30 23:41:31 +0700

    Done: AJAX, Fetching data from dicoding sport API with fetch

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -1,17 +1,17 @@
-import clubs from "./clubs.js";
-
-export default class DataSource{
-  static searchClub(keyword){
-    return new Promise ((resolve, reject) => { 
-      const filteredClubs = clubs.filter(club => {
-        return club.name.toUpperCase().includes(keyword.toUpperCase());
-      });
-
-      if (filteredClubs.length) {
-        resolve(filteredClubs);
-      } else {
-        reject(`${keyword} is not found`);
-      }
-    })
+class DataSource {
+  static searchClub(keyword) {
+    return fetch(`https://sports-api.dicoding.dev/teams/search?t=${keyword}`)
+        .then(response => {
+          return response.json();
+        })
+        .then(responseJson => {
+          if (responseJson.teams) {
+            return Promise.resolve(responseJson.teams);
+          } else {
+            return Promise.reject(`${keyword} is not found`);
+          }
+    });
   }
 }
+
+export default DataSource;
